6429e89642f431c9f7ede5dc453915fe0ef81842
narukaz <85193271+narukaz@users.noreply.github.com>
2025-06-21 01:42:33 +0530

    adding json parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./app.js";
-
+app.use(express.json());
 const port = 3000;
 app.listen(port, () => console.log(`Listening on ${port}`));
