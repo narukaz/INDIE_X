444343ff8cfd0fc5f10446d4bff15a67cad5c7d4
narukaz <85193271+narukaz@users.noreply.github.com>
2025-06-25 03:26:23 +0530

    changing cid pattern

--- server/controllers/metadataController.js
+++ server/controllers/metadataController.js
@@ -3,13 +3,8 @@ import * as pinService from "../config/pinService.js";
 export async function publishMetadata(req, res) {
   try {
     console.log("reqest body -> ", req.body);
-    const { name, description, attributes, imageCid } = req.body;
-    const metadata = {
-      name,
-      description,
-      image: `cyan-realistic-swift-995.mypinata.cloud/ipfs/${imageCid}`,
-      attributes,
-    };
+
+    const metadata = req.body;
     const metaCid = await pinService.pinJson(metadata);
     res.json({ metaCid });
   } catch (error) {
